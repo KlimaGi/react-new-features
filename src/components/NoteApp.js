3ad43ac0e457021f657caf6ed08e94a921febb36
Giedre Klimaite <giedre.klima@gmail.com>
2021-09-17 17:47:37 +0300

    useContext Hook

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -1,15 +1,11 @@
 import React, { useEffect, useReducer } from "react";
 import notesReducer from "../reducers/notes";
-
 import NoteList from "./NoteList";
 import AddNoteForm from "./AddNoteForm";
+import NotesContext from "../context/notes-context";
 
 const NoteApp = () => {
   const [notes, dispatch] = useReducer(notesReducer, []);
-
-  const removeNote = (title) => {
-    dispatch({ type: "REMOVE_NOTE", title });
-  };
 
   useEffect(() => {
     const notes = JSON.parse(localStorage.getItem("notes"));
@@ -24,12 +20,11 @@ const NoteApp = () => {
   }, [notes]);
 
   return (
-    <div>
+    <NotesContext.Provider value={{ notes, dispatch }}>
       <h2>Notes</h2>
-      <NoteList notes={notes} removeNote={removeNote} />
-
-      <AddNoteForm dispatch={dispatch} />
-    </div>
+      <NoteList />
+      <AddNoteForm />
+    </NotesContext.Provider>
   );
 };
 
